13e529f71526d328c4aa8f01d91c3643450e7339
softplan <soft.plan6874@fastmail.com>
2024-12-22 23:29:12 -0600

    logo, temp page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-medium font-geist-mono gradient-text">
-        rackplan.dev
-      </h1>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <Image
+          src="/rackplan-logo.svg"
+          alt="rackplan.dev"
+          width={60}
+          height={60}
+        />
+        <h1 className="text-4xl font-medium font-geist-mono gradient-text">
+          rackplan.dev
+        </h1>
+      </div>
     </div>
   );
 }
